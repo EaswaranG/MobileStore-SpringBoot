98d4ea71fbbc8a41c62117754bfe4f4565b23f76
Easwaran <rajkumar26041997@gmail.com>
2023-03-23 09:24:42 -0400

    Dummy Commit

--- src/main/java/com/easwar/MobileStoreApplication.java
+++ src/main/java/com/easwar/MobileStoreApplication.java
@@ -10,6 +10,7 @@ public class MobileStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileStoreApplication.class, args);
+
 	}
 
 }
